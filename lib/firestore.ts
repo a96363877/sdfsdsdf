caafb477dae7adc8a4423588b54e508530ef80ff
a96363877 <a96363877@gmail.com>
2025-06-02 04:46:34 +0300

    4544

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -4,14 +4,14 @@ import { getFirestore} from 'firebase/firestore';
 import { getDatabase} from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6YYfClSDS8JIDDLhRYQsUQb7DTrCR7_4",
-  authDomain: "new-swidah.firebaseapp.com",
-  databaseURL: "https://new-swidah-default-rtdb.firebaseio.com",
-  projectId: "new-swidah",
-  storageBucket: "new-swidah.firebasestorage.app",
-  messagingSenderId: "139797141578",
-  appId: "1:139797141578:web:85f9352e248cf25eab2eb3",
-  measurementId: "G-ZC14VFDDJ1"
+  apiKey: "AIzaSyA8uJ0_9_zgJKAIEZLxo3kwHq_3LVQD_H8",
+  authDomain: "test-2d692.firebaseapp.com",
+  databaseURL: "https://test-2d692-default-rtdb.firebaseio.com",
+  projectId: "test-2d692",
+  storageBucket: "test-2d692.firebasestorage.app",
+  messagingSenderId: "1054756802207",
+  appId: "1:1054756802207:web:fc008ce03e759bc1bd1caa",
+  measurementId: "G-RDW9YJ5MDF"
 };
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
